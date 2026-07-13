0248328eecf2138fe193bb124460193f54a9b0a5
Noah <benkendorfnoah983@gmail.com>
2026-07-13 16:49:52 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,47 @@
+async function loadPage(page){
 
+
+const content = document.getElementById("content");
+
+
+try {
+
+
+const response = await fetch(`pages/${page}.html`);
+
+
+if(!response.ok){
+throw new Error("Page not found");
+}
+
+
+const html = await response.text();
+
+
+content.innerHTML = html;
+
+
+
+lucide.createIcons();
+
+
+}
+
+catch(error){
+
+content.innerHTML =
+`
+<h1>Error</h1>
+<p>This page does not exist yet.</p>
+`;
+
+}
+
+
+}
+
+
+
+// Load homepage automatically
+
+loadPage("home");
